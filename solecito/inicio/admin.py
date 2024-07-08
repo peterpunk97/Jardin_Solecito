589736006bd75719be2529c27323e05f53d332d7
peterpunk97 <pedroandradedj@outlook.com>
2024-07-07 22:15:33 -0600

    Actualizacion 2
    creación de los paneles para administrar los modelos

--- solecito/inicio/admin.py
+++ solecito/inicio/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from.models import Tarea
+from.models import Grupo
+from.models import Evento
+from.models import Materia
 
-# Register your models here.
+
+
+class AdministrarTarea(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('idTarea', 'nombreTarea', 'fechaEntrega')
+    search_fields = ('idTarea', 'nombreTarea', 'fechaEntrega')
+    date_hierarchy = 'created'
+    list_filter = ('idTarea', 'nombreTarea', 'fechaEntrega')
+
+admin.site.register(Tarea, AdministrarTarea)
+
+class AdministrarGrupo(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('idGrupo', 'nombreGrupo', 'created')
+    search_fields = ('idGrupo', 'nombreGrupo', 'created')
+    date_hierarchy = 'created'
+    list_filter = ('idGrupo', 'nombreGrupo', 'created')
+
+admin.site.register(Grupo, AdministrarGrupo)
+
+
+class AdministrarEvento(admin.ModelAdmin):
+    readonly_fields = ('idEvento',)  
+    list_display = ('idEvento', 'nombreEvento', 'fechaEvento')
+    search_fields = ('idEvento', 'nombreEvento', 'fechaEvento')
+    date_hierarchy = 'fechaEvento'
+    list_filter = ('fechaEvento',)
+
+admin.site.register(Evento, AdministrarEvento)
+
+
+class AdministrarMateria(admin.ModelAdmin):
+    readonly_fields = ('idMateria', 'created', 'updated')  
+    list_display = ('idMateria', 'nombreMateria', 'descripcionMateria', 'created', 'updated') 
+    search_fields = ('idMateria', 'nombreMateria', 'descripcionMateria')
+    list_filter = ('created', 'updated')
+
+admin.site.register(Materia, AdministrarMateria)
--- solecito/solecito/urls.py
+++ solecito/solecito/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from inicio import views
+from django.conf import settings
 
 urlpatterns = [
     path('',views.home, name="Home"),
@@ -10,3 +11,10 @@ urlpatterns = [
     path('contacto/',views.contacto, name="Contacto"),
     path('admin/', admin.site.urls, name="Admin"),
 ]
+
+
+if settings.DEBUG:
+
+    from django.conf.urls.static import static 
+    urlpatterns += static(settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
