056ca55ae9a732b21c4e422710da88286c884603
peterpunk97 <pedroandradedj@outlook.com>
2024-07-14 23:47:09 -0600

    Modificacion del panel administrador con Jazzmin

--- solecito/inicio/admin.py
+++ solecito/inicio/admin.py
@@ -12,6 +12,9 @@ class AdministrarTarea(admin.ModelAdmin):
     search_fields = ('idTarea', 'nombreTarea', 'fechaEntrega')
     date_hierarchy = 'created'
     list_filter = ('idTarea', 'nombreTarea', 'fechaEntrega')
+    list_per_page=2
+    list_display_links=('idTarea','nombreTarea')
+    list_editable=('fechaEntrega',)
 
 admin.site.register(Tarea, AdministrarTarea)
 
@@ -21,6 +24,8 @@ class AdministrarGrupo(admin.ModelAdmin):
     search_fields = ('idGrupo', 'nombreGrupo', 'created')
     date_hierarchy = 'created'
     list_filter = ('idGrupo', 'nombreGrupo', 'created')
+    list_per_page=2
+    list_editable=('nombreGrupo',)
 
 admin.site.register(Grupo, AdministrarGrupo)
 
@@ -31,6 +36,8 @@ class AdministrarEvento(admin.ModelAdmin):
     search_fields = ('idEvento', 'nombreEvento', 'fechaEvento')
     date_hierarchy = 'fechaEvento'
     list_filter = ('fechaEvento',)
+    list_per_page=2
+    list_editable=('fechaEvento',)
 
 admin.site.register(Evento, AdministrarEvento)
 
@@ -40,5 +47,7 @@ class AdministrarMateria(admin.ModelAdmin):
     list_display = ('idMateria', 'nombreMateria', 'descripcionMateria', 'created', 'updated') 
     search_fields = ('idMateria', 'nombreMateria', 'descripcionMateria')
     list_filter = ('created', 'updated')
+    list_per_page=2
+    list_editable=('descripcionMateria','nombreMateria')
 
 admin.site.register(Materia, AdministrarMateria)
