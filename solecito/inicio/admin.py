099758ea9e8f4f28a53aa48516b9439c9f3309bd
peterpunk97 <pedroandradedj@outlook.com>
2024-08-19 15:25:39 -0600

    Corrección de modelos y Admin
    se pasaron algunas de las variables a español para que la interpretación del admin a los usuarios sea en español y no genere confusiones

--- solecito/inicio/admin.py
+++ solecito/inicio/admin.py
@@ -8,20 +8,20 @@ from.models import Comentario
 
 
 class AdministrarComentario(admin.ModelAdmin):
-    readonly_fields = ('nombre', 'correo', 'telefono', 'mensaje','creado','actualizado','grupos')  
+    readonly_fields = ('nombre', 'correo', 'telefono', 'mensaje','creado','actualizado','grupos',)  
     list_display = ('nombre', 'correo', 'telefono', 'mensaje', 'grupos','respuesta') 
     search_fields = ('nombre','correo', 'telefono','creado','grupos')
     list_filter = ('nombre','creado','grupos')
     list_editable = ('respuesta',)
-    list_per_page=6
+    list_per_page=10
 admin.site.register(Comentario, AdministrarComentario)
 
 
 class AdministrarTarea(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated')
+    readonly_fields = ('creado', 'actualizado')
     list_display = ('idTarea', 'nombreTarea', 'fechaEntrega','imagen',)
     search_fields = ('idTarea', 'nombreTarea', 'fechaEntrega')
-    date_hierarchy = 'created'
+    date_hierarchy = 'creado'
     list_filter = ('idTarea', 'nombreTarea', 'fechaEntrega')
     list_per_page = 10
     list_display_links = ('idTarea', 'nombreTarea')
@@ -31,11 +31,11 @@ admin.site.register(Tarea, AdministrarTarea)
 
 
 class AdministrarGrupo(admin.ModelAdmin):
-    readonly_fields = ('created', 'updated')
-    list_display = ('idGrupo', 'nombreGrupo', 'created')
-    search_fields = ('idGrupo', 'nombreGrupo', 'created')
-    date_hierarchy = 'created'
-    list_filter = ('idGrupo', 'nombreGrupo', 'created')
+    readonly_fields = ('creado', 'actualizado',)
+    list_display = ('idGrupo', 'nombreGrupo', 'creado',)
+    search_fields = ('idGrupo', 'nombreGrupo', 'creado',)
+    date_hierarchy = 'creado'
+    list_filter = ('idGrupo', 'nombreGrupo', 'creado',)
     list_per_page=5
     list_editable=('nombreGrupo',)
 
@@ -56,10 +56,10 @@ admin.site.register(Evento, AdministrarEvento)
 
 
 class AdministrarMateria(admin.ModelAdmin):
-    readonly_fields = ('idMateria', 'created', 'updated')  
-    list_display = ('idMateria', 'nombreMateria', 'descripcionMateria', 'created', 'updated') 
+    readonly_fields = ('idMateria', 'creado', 'actualizado')  
+    list_display = ('idMateria', 'nombreMateria', 'descripcionMateria', 'creado', 'actualizado') 
     search_fields = ('idMateria', 'nombreMateria', 'descripcionMateria')
-    list_filter = ('created', 'updated')
+    list_filter = ('creado', 'actualizado')
     list_per_page=4
     list_editable=('descripcionMateria','nombreMateria')
 admin.site.register(Materia, AdministrarMateria)
--- solecito/inicio/models.py
+++ solecito/inicio/models.py
@@ -3,13 +3,13 @@ from django.db import models
 class Grupo(models.Model):
     idGrupo = models.AutoField(primary_key=True, verbose_name="ID GRUPO")
     nombreGrupo = models.CharField(max_length=50, verbose_name="Nombre del grupo")
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Grupo"
         verbose_name_plural = "Grupos"
-        ordering = ["created"]
+        ordering = ["creado"]
 
     def __str__(self):
         return self.nombreGrupo
@@ -20,13 +20,13 @@ class Materia(models.Model):
     nombreMateria = models.CharField(max_length=200, verbose_name="Nombre de la Materia")
     descripcionMateria = models.TextField(verbose_name="Agrega una descripción")
     imagen = models.ImageField(null=True, blank=True, upload_to="img-materias/", verbose_name="Imagen de la materia")
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Materia"
         verbose_name_plural = "Materias"
-        ordering = ["created"]
+        ordering = ["creado"]
 
     def __str__(self):
         return self.nombreMateria
@@ -38,22 +38,22 @@ class Tarea(models.Model):
     descripcion = models.TextField(verbose_name="Agrega una descripción")
     fechaEntrega = models.DateField(null=True, blank=True, verbose_name="Fecha de entrega")
     imagen = models.ImageField(null=True, upload_to="img-tareas", verbose_name="Imagen")
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
     grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE, verbose_name="Grupo")
     materia = models.ForeignKey(Materia, on_delete=models.CASCADE, verbose_name="Materia")
 
     class Meta:
         verbose_name = "Tarea"
         verbose_name_plural = "Tareas"
-        ordering = ["created"]
+        ordering = ["creado"]
 
     def __str__(self):
         return self.nombreTarea
 
 
 class Evento(models.Model):
-    idEvento = models.AutoField(primary_key=True)
+    idEvento = models.AutoField(primary_key=True)  
     nombreEvento = models.CharField(max_length=200, verbose_name="Nombre del evento")
     fechaEvento = models.DateField(verbose_name="Fecha del evento")
     imagen = models.ImageField(null=True, blank=True, upload_to="img-eventos/", verbose_name="Imagen del evento")
@@ -67,17 +67,17 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.nombreEvento
-
+    
 
 class Comentario(models.Model):
-    nombre = models.CharField(max_length=100, verbose_name="Nombre del padre de familia")
-    correo = models.EmailField(max_length=254, verbose_name="Correo electronico")
+    nombre = models.CharField(max_length=100, verbose_name="Nombre del padre de familia")  
+    correo = models.EmailField(max_length=254, verbose_name="Correo electronico")  
     telefono = models.CharField(max_length=15, blank=True, null=True, verbose_name="Telefono")
     mensaje = models.TextField()
     respuesta = models.TextField(blank=True, null=True, verbose_name="Ingresa tu respuesta")
     creado = models.DateTimeField(auto_now_add=True)
     actualizado = models.DateTimeField(auto_now=True)
-    grupos = models.CharField(max_length=4, verbose_name="Nombre del grupo")
+    grupos = models.CharField(max_length=4, verbose_name="Nombre del grupo") 
 
 
     class Meta:
